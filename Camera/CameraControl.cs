8a2f648af3bb75c6e6ae416e552bee17c5278e93
Trackbool <mistert430@gmail.com>
2019-02-23 21:21:10 +0100

    Add camera control script

---
+++ Camera/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Transform playerTransform;
+    public Vector3 cameraOffset = new Vector3(0,3,5);
+    public float yPosition = 0;
+
+    public float yaw = 180f;
+    public float pitch = 20f;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+        transform.position = playerTransform.position + cameraOffset; 
+        transform.rotation = new Quaternion(
+            transform.rotation.x, 
+            transform.rotation.y-yPosition, 
+            transform.rotation.z, transform.rotation.w);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
